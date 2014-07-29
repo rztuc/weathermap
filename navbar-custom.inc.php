7e54bb6cf25e48701b8c7da2b0be8bc45c581f1e
laf <neil@lathwood.co.uk>
2014-07-29 11:35:29 +0100

    Create navbar-custom.inc.php

---
+++ navbar-custom.inc.php
@@ -0,0 +1,28 @@
+<?php
+ 
+$rendered_maps = array();
+if ($weathermap_dir = opendir('weathermap/maps/')) { //Open directory
+    while (false !== ($weathermap_file = readdir($weathermap_dir))) { //while there are files to process
+        if (strpos($weathermap_file,'.html')) { //if filename contains the string ".html"
+            $weathermap_name = str_replace('.html','',$weathermap_file); //save filename and strip file ending
+            $rendered_maps[$weathermap_file] = $weathermap_name; //add filename and mapname in array
+        }
+    }
+    closedir($weathermap_dir);
+}
+ 
+$navbar['observium']['entries'][] = array('divider' => TRUE);
+ 
+foreach ($rendered_maps as $map_page => $map_name)
+{
+  $weathermap_menu[] = array('title' => $map_name, 'url' => 'weathermap/maps/' . $map_page, 'icon' => 'oicon-map');
+}
+ 
+$navbar['observium']['entries'][] = array('title' => 'Weathermaps', 'url' => generate_url(array('page' => 'weathermap')), 'icon' => 'oicon-map', 'entries' => $weathermap_menu);
+ 
+if ($_SESSION['userlevel'] >= '10')
+{
+  $navbar['observium']['entries'][] = array('title' => 'Weathermap Editor', 'url' => 'weathermap/editor.php', 'icon' => 'oicon-map');
+}
+ 
+?>
